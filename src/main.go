39fac459bd76e5c5dc36d553a3abb6a6804c1539
Gregory Potemkin <greg@pop-os.localdomain>
2021-03-09 01:33:35 +0300

    fd

--- src/main.go
+++ src/main.go
@@ -34,14 +34,13 @@ func main() {
 		echoPort := binary.BigEndian.Uint32(buf[:4])
 		addr.Port = int(echoPort)
 
-		fmt.Printf("Writing response to %v", *addr)
-
-		fmt.Printf("received: '%s' from: %s\n%v\n", string(buf[4:n]), addr.String(), time.Now())
+		fmt.Printf("%d bytes received: '%s' from: %s\n%s\n", n, string(buf[4:n]), addr.String(), time.Now())
+		fmt.Printf("Writing response to %v\n", *addr)
 
 		if err != nil {
 			fmt.Println("error: ", err)
 		}
-		
+
 		ServerConn.WriteTo(buf[0:n], addr)
 	}
 }
